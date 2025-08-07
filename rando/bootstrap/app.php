6c0c10ef5718ed1b1339755ff5252fd748d9a2b3
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-08-07 05:45:15 +0200

    modif app.php(kernel) + api.php

--- rando/bootstrap/app.php
+++ rando/bootstrap/app.php
@@ -1,16 +1,35 @@
 <?php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+
 return Application::configure(basePath: dirname(__DIR__))
-->withRouting(
-web: __DIR__.'/../routes/web.php',
-commands: __DIR__.'/../routes/console.php',
-health: '/up',
-api: __DIR__.'/../routes/api.php'
-)
-->withMiddleware(function (Middleware $middleware) {
-})
-->withExceptions(function (Exceptions $exceptions) {
-//
-})->create();
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        api: __DIR__.'/../routes/api.php'
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+
+        $middleware->group('web', [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            VerifyCsrfToken::class,
+            SubstituteBindings::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        // Configuration des exceptions ici
+    })
+    ->create();
